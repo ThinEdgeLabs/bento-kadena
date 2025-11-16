f58e28694a66dc88cc2fcec2785798d439d0403f
Radu Ciobanu <radu.ciobanu22@gmail.com>
2025-11-16 21:39:14 +0800

    Fixed migration

--- migrations/2025-11-16-114655_add_balance_query_indexes/up.sql
+++ migrations/2025-11-16-114655_add_balance_query_indexes/up.sql
@@ -1,7 +1,7 @@
 -- Add indexes optimized for balance calculation queries
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS transfers_from_acct_module_chain_idx
+CREATE INDEX IF NOT EXISTS transfers_from_acct_module_chain_idx
   ON transfers (from_account, module_name, chain_id);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS transfers_to_acct_module_chain_idx
+CREATE INDEX IF NOT EXISTS transfers_to_acct_module_chain_idx
   ON transfers (to_account, module_name, chain_id);
